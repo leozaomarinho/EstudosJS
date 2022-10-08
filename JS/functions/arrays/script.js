c5af002c9a699bc857ea6bc4c3ff451bb053a75d
Leonardo Marinho <81639502+leozaomarinho@users.noreply.github.com>
2022-10-08 10:58:49 -0300

    treinando arrays e native functions

---
+++ JS/functions/arrays/script.js
@@ -0,0 +1,32 @@
+
+let nomes =['raquel','be','leo']
+
+for(let i= 0; i<=nomes.length;i++){
+
+   
+    console.log(nomes[i]);
+}
+
+let Qpessoas = prompt('digite a quantidade de pessoas:')
+
+let i =0;5
+
+while(i<=Qpessoas){
+    
+    console.log(i)
+    i++
+}
+
+
+let pessoas= ['leo', 'raquel','julio', 'thays'];
+
+let PessoaRemovida = pessoas.pop(3);
+
+console.log(pessoas);
+
+console.log(PessoaRemovida);
+
+let PessoaRemovida2 = pessoas.pop(2);
+
+console.log(PessoaRemovida2);
+
