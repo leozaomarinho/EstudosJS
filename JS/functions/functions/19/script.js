54b06b3faa7eb845a44c27901c77468fb8b16573
Leonardo Marinho <81639502+leozaomarinho@users.noreply.github.com>
2022-10-14 13:32:49 -0300

    add files

---
+++ JS/functions/functions/19/script.js
@@ -0,0 +1,6 @@
+
+function numAleatorio(numMax){
+    return Math.floor(Math.random()* numMax+1)
+}
+
+console.log(numAleatorio(100));
---
+++ JS/functions/functions/20/script.js
@@ -0,0 +1,13 @@
+let idade = 17;
+
+function verificaIdade(idade){
+    if(idade>=18){
+        console.log('você pode entrar na auto escola.')
+    }
+    else{
+        console.log('você não pode entrar na auto escola')
+    }
+};
+
+console.log(verificaIdade(idade));
+
