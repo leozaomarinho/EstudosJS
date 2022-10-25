56d2a06b0989e728447066a930dad6e6938ab098
Leonardo Marinho <81639502+leozaomarinho@users.noreply.github.com>
2022-10-25 16:05:25 -0300

    add files

---
+++ JS/ObjetosAtividades/05/script.js
@@ -0,0 +1,69 @@
+class ContaBancaria{
+    constructor(saldoCC,saldoCP,jurosCP){
+        this.saldoCC = saldoCC;
+        this.saldoCP = saldoCP;
+        this.jurosCP = jurosCP;
+    }
+
+
+    set depositoCC(valor){
+        this.saldoCC+=valor;
+        console.log(`você realizou um deposito no valor de R$${valor} reais.`);
+        console.log(`Seu saldo atual na conta corrente é de R$${this.saldoCC}`)
+    }
+
+    set depositoCP(valor){
+        this.saldoCP+=valor;
+
+        console.log(`você realizou um deposito no valor de R$${valor} reais.`);
+        console.log(`Seu saldo atual na conta poupança é de R$${this.saldoCP}`);
+    }
+
+     transferir(valor,contaEnvio,contaReceb){
+
+    if(contaEnvio === 'ContaPP' && contaReceb === 'ContaCC'){    
+//transf de Conta Poupança para conta Corrente
+        if(valor<=saldoCP){
+        this.saldoCC+=this.saldoCP;
+        saldoCP -=valor;
+        console.log('Transfêrencia realizada com sucesso!')
+        console.log(`O seu novo saldo total na conta corrente é de R$${this.saldoCC}`);
+        }
+        else{
+            console.log(`O valor a ser transferido não pode ser maior que o saldo atual! seu saldo é ${saldoCP}`)
+        }
+    }
+
+    else if(contaEnvio === 'ContaCC' && contaReceb === 'ContaPP'){
+//transf de conta corrente para conta poupança
+        if(valor<=this.saldoCC){
+            this.saldoCP+=this.saldoCC;
+            this.saldoCC-=valor;
+            console.log('Transfêrencia realizada com sucesso!')
+            console.log(`O seu novo saldo total na conta poupança é de R$${this.saldoCP}`);
+            }
+            else{
+                console.log(`O valor a ser transferido não pode ser maior que o saldo atual! seu saldo é ${saldoCP}`)
+            }
+    }
+    
+     //fim do metodo transfência
+    }
+   
+
+    }
+
+
+    class ContaEspecial extends ContaBancaria{
+
+    }
+
+    let conta2 = new ContaEspecial(350,80,4)
+
+    let conta = new ContaBancaria(400,100,2)
+
+    console.log(conta);
+    console.log(conta2)
+
+    console.log(Object.prototype(conta2))
+    
