b64dea0b87633662ad78c4c51c041847ccf3758a
Leonardo Marinho <81639502+leozaomarinho@users.noreply.github.com>
2022-11-12 13:30:29 -0300

    update files, and new functions

--- JS/DOM/02/script.js
+++ JS/DOM/02/script.js
@@ -1,5 +1,10 @@
 let novoElemento = document.createElement('p');
 let elementoAlvo = document.querySelector('.titulo-principal');
 let elementoPai = document.querySelector('#container-principal')
+let texto = document.createTextNode('adicionando textos com o DOM')
+
+elementoPai.insertBefore(novoElemento,elementoAlvo);
+
+novoElemento.appendChild(texto);
 
 elementoPai.insertBefore(novoElemento,elementoAlvo);
